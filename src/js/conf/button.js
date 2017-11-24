60f40d390dce9da6938c666edfc595717263c498
unknown <jiasl@TECH.BITAUTO.COM>
2017-11-24 09:53:35 +0800

    98989

--- src/js/conf/button.js
+++ src/js/conf/button.js
@@ -3,43 +3,45 @@ const Photoclip=require('photoclip');
 const html2canvas=require('html2canvas/dist/html2canvas');
 const block=$('#block');
 const area=$('#clipArea');
-const btn=$('#clipBtn');
+const ok=$('#clipBtn');
 const view=$('#view');
 const can=$('#canvas');
 const img=$('#img');
+const file=$('#file');
 const photo=new Photoclip(area,{
-    // size: 260,
     adaptive: ['100%', '100%'],
     outputSize: 640,
-    //adaptive: ['60%', '80%'],
-    file: '#file',
-    view: '#view',
-    ok: '#clipBtn',
+    file,
+    view,
+    ok,
     maxZoom: "2",
     // img: 'img/mm.jpg',
     loadStart: function() {
+        view.css('display','none');
         console.log('开始读取照片');
     },
     loadComplete: function() {
         console.log('照片读取完成');
+
     },
     done: function(dataURL) {
-        console.log(dataURL);
-
+        view.css('display','block');
+        html2canvas(block).then((canvas)=>{
+            can.append(canvas);
+            var a1='../../images/1.jpg';
+            img.on('load',()=>{
+                const a1=canvas.toDataURL('image/png');
+                img.attr('src',a1);
+            });
+            img.attr('src',a1);           
+        });
     },
     fail: function(msg) {
         alert(msg);
     }
 });
-btn.on('click',()=>{
-	view.css('display','block');
-});
-html2canvas(block).then((canvas)=>{
-        can.append(canvas);
-        var a1='../images/1.jpg';
-        img.on('load',()=>{
-            const a1=canvas.toDataURL('image/png');
-            img.attr('src',a1);
-        });
-        img.attr('src',a1);           
-    });
+
+
+
+
+
